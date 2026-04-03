9c59ca586417b168705e0199063c4d7b6f9cdc14
Jorge Sánchez <133137151+GeorgeSan97@users.noreply.github.com>
2026-04-02 21:48:15 -0500

    Corrección de programa

--- NorthWind.Sales.WebApi/obj/Debug/net8.0/NorthWind.Sales.WebApi.AssemblyInfo.cs
+++ NorthWind.Sales.WebApi/obj/Debug/net8.0/NorthWind.Sales.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NorthWind.Sales.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6048f4f76f7fedd03e611db76ee5e0246bf7ba0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944b293d3746c63b161fd107304b1e39ea5b8faf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NorthWind.Sales.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NorthWind.Sales.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
